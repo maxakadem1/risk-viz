42044827e2d39d0ccb27ae9a88b424493cfa3a85
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2023-04-23 17:21:11 -0500

    added markers to the map

--- src/app/mapbox.tsx
+++ src/app/mapbox.tsx
@@ -1,12 +1,61 @@
 "use client";
-import mapboxgl, { Marker, Popup } from "mapbox-gl";
+import mapboxgl from "mapbox-gl";
 import React, { useEffect, useState } from "react";
 import "./style/mapbox.css";
 
-const Map = () => {
+interface LocationData {
+  name: string;
+  lat: number;
+  long: number;
+  category: string;
+  rating: number;
+  factors: Record<string, any>;
+  year: number;
+}
+
+const Map: React.FC = () => {
   // mapbox token access
   mapboxgl.accessToken =
     "pk.eyJ1IjoibWF4LW5hdmVybiIsImEiOiJjbGdyNHZvbnMxZnlsM2ZwMzhtMnJlbGx5In0.AFyKCpWprEwqoTKq4Ezk7Q";
+
+  // default selection
+  const [selectedYear, setSelectedYear] = useState<number>(2030);
+  const [map, setMap] = useState<mapboxgl.Map | null>(null);
+
+  const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedYear(parseInt(event.target.value, 10));
+  };
+
+  const buildMap = (filteredData: LocationData[]) => {
+    if (map) {
+      // Remove any existing markers
+      map.remove();
+
+      // Create a new map instance
+      const newMap = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/dark-v11",
+        center: [-74.5, 40],
+        zoom: 1,
+      });
+
+      // Set the new map instance in state
+      setMap(newMap);
+
+      // Add markers to the new map
+      filteredData.forEach((location) => {
+        const markerColor =
+          location.rating > -100
+            ? "green"
+            : location.rating > -50
+            ? "yellow"
+            : "red";
+        new mapboxgl.Marker({ color: markerColor })
+          .setLngLat([location.long, location.lat])
+          .addTo(newMap);
+      });
+    }
+  };
 
   // fetch data from CSV file
   useEffect(() => {
@@ -35,19 +84,44 @@ const Map = () => {
         };
       });
 
-      // add markers to the map based on the location coordinates
-      const map = new mapboxgl.Map({
-        container: "map",
-        style: "mapbox://styles/mapbox/dark-v11",
-        center: [-74.5, 40], // starting position [lng, lat]
-        zoom: 1, // starting zoom
-      });
+      const filteredData = json.filter((item) => item.year === selectedYear);
+      console.log(filteredData);
+
+      if (!map) {
+        // Initialize the map for the first time
+        const initialMap = new mapboxgl.Map({
+          container: "map",
+          style: "mapbox://styles/mapbox/dark-v11",
+          center: [-74.5, 40],
+          zoom: 1,
+        });
+
+        setMap(initialMap);
+      } else {
+        // Filter data and update the map
+        const filteredData = json.filter((item) => item.year === selectedYear);
+        buildMap(filteredData);
+      }
     };
 
     fetchData();
-  }, []);
+  }, [selectedYear]);
 
-  return <div id="map" style={{ width: "100%" }} />;
+  return (
+    <div style={{ width: "100%" }}>
+      <div>
+        <label htmlFor="year">Select Year: </label>
+        <select id="year" value={selectedYear} onChange={handleYearChange}>
+          <option value="2030">2030</option>
+          <option value="2040">2040</option>
+          <option value="2050">2050</option>
+          <option value="2060">2060</option>
+          <option value="2070">2070</option>
+        </select>
+      </div>
+      <div id="map" style={{ width: "100%" }} />
+    </div>
+  );
 };
 
 export default Map;
